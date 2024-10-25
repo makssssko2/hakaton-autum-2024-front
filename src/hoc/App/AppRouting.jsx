5d46d99edc780e363f78c157a8ff071ae64a707b
mayonezny <borischubuhchiev@mail.ru>
2024-10-25 21:43:27 +0300

    routes

--- src/hoc/App/AppRouting.jsx
+++ src/hoc/App/AppRouting.jsx
@@ -1,12 +1,16 @@
 import {Route, Routes} from "react-router-dom";
 import MainPage from "../../pages/MainPage/HomePage.jsx";
 import LoginPage from "../../pages/LoginPage/LoginPage.jsx";
+import DeskPage from "../../pages/DeskPage/DeskPage.jsx";
+import RegistrationPage from "../../pages/RegistrationPage/RegistrationPage.jsx";
 
 const AppRouting = () => {
     return (
         <Routes>
             <Route path="" element={<MainPage />} />
             <Route path="/login" element={<LoginPage />} />
+            <Route path="/registration" element={<RegistrationPage />} />
+            <Route path="/desk" element={<DeskPage />} />
         </Routes>
     )
 }
---
+++ src/pages/DeskPage/DeskPage.jsx
@@ -0,0 +1,11 @@
+import Layout from "../../layouts/Layout/Layout.jsx";
+const DeskPage = () => {
+
+    return (
+        <Layout type={'wide'}>
+            Zapara lox
+        </Layout>
+    )
+}
+
+export default DeskPage;
