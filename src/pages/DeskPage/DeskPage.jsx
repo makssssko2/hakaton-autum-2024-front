13a6f86d770b0820af69bc8df594e54d747d32db
makssssko <grinkomaks25@gmail.com>
2024-10-26 16:46:43 +0300

    fix adaptive height

--- src/pages/DeskPage/DeskPage.jsx
+++ src/pages/DeskPage/DeskPage.jsx
@@ -3,6 +3,7 @@ import Header from "../../components/Header/Header.jsx";
 import Canban from "../../components/Canban/Canban.jsx";
 import './DeskPage.scss';
 const DeskPage = () => {
+    return (
         <Layout type={'wide'} className={'DeskPage'}>
             <Header/>
             <Canban/>
